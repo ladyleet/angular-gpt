91910b1415e009576aff60a22eadc10b230925ca
Tracy <ladyleet@Tracys-MacBook-Pro.local>
2024-03-19 00:04:53 -0400

    ending for the nite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { MatCardModule } from '@angular/material/card';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatCardModule, MatSlideToggleModule, MatCheckboxModule, MatButtonModule],
+  imports: [RouterOutlet, MatCardModule, MatSlideToggleModule, MatCheckboxModule, MatButtonModule, MatFormFieldModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
