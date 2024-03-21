de91501ad21e30fdf833f7ce3732a236e1411fc0
Tracy <ladyleet@Tracys-MacBook-Pro.local>
2024-03-21 15:27:21 -0400

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,43 +1,61 @@
-import { Component, Injector, inject } from '@angular/core';
+import { Component, Injector, computed, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatButtonModule} from '@angular/material/button';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatCardModule, MatSlideToggleModule, MatCheckboxModule, MatButtonModule, MatFormFieldModule, ReactiveFormsModule, MatInputModule, HttpClientModule],
+  imports: [
+    RouterOutlet,
+    MatCardModule,
+    MatSlideToggleModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    HttpClientModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-scss-again';
   angularQuestionForm = new FormGroup({
-    question: new FormControl('')
+    question: new FormControl(''),
   });
   httpClient = inject(HttpClient);
-  
+  results = signal<object>([]);
+  resultsJSON = computed(() => {
+    return JSON.stringify(this.results());
+  });
   sendRequest(message: string) {
-    try{
-     (this.httpClient.post("http://localhost:3000/api/chat",{messages:[{content:message}]}).subscribe((data) => {console.log(data)}));
-    }
-    catch(e){
+    try {
+      this.httpClient
+        .post('http://localhost:3000/api/chat', {
+          messages: [{ content: message }],
+        })
+        .subscribe((data) => {
+          this.results.set(data);
+        });
+    } catch (e) {
       console.log(e);
     }
   }
 
   onSubmitForForm() {
     console.log(this.angularQuestionForm.value);
-    this.sendRequest(this.angularQuestionForm.value.question||'');  
+    this.sendRequest(this.angularQuestionForm.value.question || '');
   }
 
   onClickForPrePromptButtons(message: string) {
-    this.sendRequest(message);  
+    this.sendRequest(message);
   }
 }
